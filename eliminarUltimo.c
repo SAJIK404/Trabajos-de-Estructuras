e1ce871b449128d975682ea74b82c7c35cbfcece
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-19 19:23:33 -0600

    Actualizar eliminarUltimo.c

--- eliminarUltimo.c
+++ eliminarUltimo.c
@@ -7,3 +7,34 @@ typedef struct Nodo {
     struct Nodo* siguiente;
 } Nodo;
 // fin estructura Nodo
+
+// INICIO FUNCION eliminarUltimo
+void eliminarUltimo(Nodo **cabeza){
+    if (*cabeza == NULL){
+        printf("La lista está vacía.\n");
+        return;
+    }
+
+    if ((*cabeza)->siguiente == NULL){
+        printf("Eliminando único nodo con valor %d.\n", (*cabeza)->dato);
+        free(*cabeza);
+        *cabeza = NULL;
+        return;
+    }
+
+    Nodo *actual = *cabeza;
+    while (actual->siguiente->siguiente != NULL){
+        actual = actual->siguiente;
+    }
+
+    printf("Eliminando último nodo con valor %d.\n", actual->siguiente->dato);
+    free(actual->siguiente);
+    actual->siguiente = NULL;
+}
+// FIN INICIO eliminarUltimo
+
+int main(){
+    Nodo *lista1 = NULL;
+    eliminarUltimo(&lista1);
+    return 0;
+}
