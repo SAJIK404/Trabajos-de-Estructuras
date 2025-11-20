dacb214f9a479cd40b1aa6fdf24ad8a52e108be4
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-19 19:54:32 -0600

    Actualizar buscar.c

--- buscar.c
+++ buscar.c
@@ -7,3 +7,28 @@ typedef struct Nodo {
     struct Nodo* siguiente;
 } Nodo;
 // fin estructura Nodo
+
+
+// INICIO FUNCION buscar
+void buscar(Nodo *cabeza, int valor){
+    Nodo *actual = cabeza;
+    int posicion = 0;
+
+    while (actual != NULL){
+        if (actual->dato == valor){
+            printf("Valor %d encontrado en la posición %d.\n", valor, posicion);
+            return;
+        }
+        actual = actual->siguiente;
+        posicion++;
+    }
+
+    printf("Valor %d NO encontrado en la lista.\n", valor);
+}
+// FIN FUNCION buscar
+
+int main(){
+    Nodo *lista1 = NULL;
+    buscar(*lista1, 666);
+    return 0;
+}
