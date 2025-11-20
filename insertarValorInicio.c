0a93d12288e391abe5c288c5692672722d58abea
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-19 18:46:37 -0600

    Actualizar insertarValorInicio.c

--- insertarValorInicio.c
+++ insertarValorInicio.c
@@ -29,9 +29,9 @@ void insertarValorInicio(Nodo **cabeza, int valor){
 
 
 int main(){
-    Nodo *lista1 = NULL
-    Nodo *lista2 = NULL
-    insertarValorInicio(&lista1, 666)
+    Nodo *lista1 = NULL;
+    Nodo *lista2 = NULL;
+    insertarValorInicio(&lista1, 666);
 
     return 0
 }
