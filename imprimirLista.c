acb0ecfb9583acb1c05963c99ef6177802948544
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-19 19:48:39 -0600

    Actualizar imprimirLista.c

--- imprimirLista.c
+++ imprimirLista.c
@@ -7,3 +7,22 @@ typedef struct Nodo {
     struct Nodo* siguiente;
 } Nodo;
 // fin estructura Nodo
+
+// INICIO FUNCION imprimirLista
+void imprimirLista(Nodo *cabeza){
+    Nodo *actual = cabeza;
+
+    printf("Lista: ");
+    while (actual != NULL){
+        printf("%d -> ", actual->dato);
+        actual = actual->siguiente;
+    }
+    printf("NULL\n");
+}
+// FIN FUNCION imprimirLista
+
+int main(){
+    Nodo *lista1 = NULL;
+    imprimirLista(&lista1);
+    return 0;
+}
