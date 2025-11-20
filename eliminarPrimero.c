f916d82cb94374b5f659b4d438ae6981a76a5c39
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-19 19:12:37 -0600

    Actualizar eliminarPrimero.c

--- eliminarPrimero.c
+++ eliminarPrimero.c
@@ -1,1 +1,17 @@
-//t
+
+
+
+// INICIO FUNCION eliminarPrimero
+void eliminarPrimero(Nodo **cabeza){
+    if (*cabeza == NULL){
+        printf("La lista está vacía.\n");
+        return;
+    }
+
+    Nodo *temp = *cabeza;
+    *cabeza = (*cabeza)->siguiente;
+
+    printf("Eliminando primer nodo con valor %d.\n", temp->dato);
+    free(temp);
+}
+// FIN FUNCION eliminarPrimero
