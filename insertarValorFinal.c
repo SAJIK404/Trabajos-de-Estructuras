986ca0195954cfc2614a327b8c91c0b45da011d5
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-19 19:04:50 -0600

    Actualizar insertarValorFinal.c

--- insertarValorFinal.c
+++ insertarValorFinal.c
@@ -7,3 +7,35 @@ typedef struct Nodo {
     struct Nodo* siguiente;
 } Nodo;
 // fin estructura Nodo
+
+// INICIO FUNCION insertarValorFinal
+void insertarValorFinal(Nodo **cabeza, int valor){
+    Nodo *nuevoNodo = (Nodo*) malloc(sizeof(Nodo));
+    if (!nuevoNodo){
+        printf("Error: no se pudo asignar memoria.\n");
+        return;
+    }
+
+    nuevoNodo->dato = valor;
+    nuevoNodo->siguiente = NULL;
+
+    if (*cabeza == NULL){
+        *cabeza = nuevoNodo;
+        printf("Insertado %d al final (primer nodo).\n", valor);
+        return;
+    }
+
+    Nodo *actual = *cabeza;
+    while (actual->siguiente != NULL){
+        actual = actual->siguiente;
+    }
+
+    actual->siguiente = nuevoNodo;
+    printf("Insertado %d al final.\n", valor);
+}
+// FIN FUNCION insertarValorFinal
+
+int main(){
+    Nodo *lista1 = NULL;
+    insertarValorInicio(&lista1, 666)
+    return 0;
